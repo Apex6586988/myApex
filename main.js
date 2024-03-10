733fe8c50bad09adf5a0f8491534c799a9f19683
Apex6586988 <162894948+Apex6586988@users.noreply.github.com>
2024-03-10 15:33:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 const d = new Date();
 let string = d.toLocaleDateString()+'_'+d.toLocaleTimeString()
 
-fs.appendFile(string+'.txt', 'File created at ' + string, function (err) {
+fs.appendFile('test.txt', string, function (err) {
     if (err) throw err;
     console.log('Saved ' + string + '!');
 });
